5480daf5e4736527681bf7865cd4935058de2dac
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-08-31 21:41:36 -0600

    eloquent js: finish ch6 notes

--- eloquent_javascript/6_secret_life_of_objects/notes.mjs
+++ eloquent_javascript/6_secret_life_of_objects/notes.mjs
@@ -103,25 +103,138 @@ console.log([1, 2].toString());
 console.log(Object.prototype.toString.call([1,2]));
 
 // Map - data structure associates keys with other values
-let ages = {
+let agesCustom = {
     Boris: 39,
     Liang: 22,
     Julia: 62
 }
-console.log(`Julia is ${ages["Julia"]}`);
-console.log("Is Jack's age known?", "Jack" in ages);
-console.log("Is toString's age known?", "toString" in ages);
+console.log(`Julia is ${agesCustom["Julia"]}`);
+console.log("Is Jack's age known?", "Jack" in agesCustom);
+console.log("Is toString's age known?", "toString" in agesCustom);
 
 // So, we can see using plain objects as maps is dangeorus
 // Ways to avoid: 1) create object with no prototype
 console.log("toString" in Object.create(null));
 
 // Map class from JavaScript - get, set and has
-//let ages = new Map();
+let ages = new Map();
 ages.set("Boris", 39);
 ages.set("Liang", 22);
 ages.set("Julia", 62);
 
-console.log(`Julia is ${ages.get(Julia)}`);
+console.log(`Julia is ${ages.get("Julia")}`);
 console.log("Is Jack's age known?", ages.has("Jack"));
 console.log(ages.has("toString"));
+
+// If for some reason we do need to treat Object as map
+// can use Object.keys to return the keys of that Object
+// and not the prototype
+console.log({x:1}.hasOwnProperty("x"));
+console.log({x:1}.hasOwnProperty("toString"));
+
+// Polymorphism: code working with values of different shapes
+Rabbit.prototype.toString = function() {
+    return `a ${this.type} rabbit`;
+};
+console.log(String(blackRabbit));
+
+// Symbols - values that are unique
+let sym = Symbol("name");
+console.log(sym == Symbol("name"));
+Rabbit.prototype[sym] = 55;
+console.log(blackRabbit[sym]);
+// Good for defining interfaces that can live alongside other properties
+const toStringSymbol = Symbol('toString');
+Array.prototype[toStringSymbol] = function() {
+    return `${this.length} cm of blue yarn`;
+};
+
+console.log([1, 2].toString());
+console.log([1,2][toStringSymbol]());
+// Include symbol properties in object expressions/classes using []
+let stringObject = {
+    [toStringSymbol]() { return "a jute rope"; }
+};
+console.log(stringObject[toStringSymbol]());
+
+// Iterator interfaces
+// Object is considered iterable if it has a Symbol.iterator
+let okIterator = "OK"[Symbol.iterator]();
+console.log(okIterator.next());
+console.log(okIterator.next());
+console.log(okIterator.next());
+
+// Implementing an iterable data structure
+class Matrix {
+    // Act as a two-dimensional array
+    constructor(width, height, element = (x, y) => undefined) {
+        this.width = width;
+        this.height = height;
+        this.content = [];
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                this.content[y*width + x] = element(x, y);
+            }
+        }
+    }
+    get(x, y) { return this.content[y* this.width + x]; }
+    set(x, y, value) { this.content[y * this.width + x] = value; }
+}
+
+class MatrixIterator {
+    constructor(matrix) {
+        this.x = 0;
+        this.y = 0;
+        this.matrix = matrix;
+    }
+
+    next() {
+        if (this.y == this.matrix.height) return {done: true};
+
+        let value = {x: this.x, y:this.y, value: this.matrix.get(this.x, this.y)};
+        this.x++;
+        if (this.x == this.matrix.width) {
+            this.x = 0;
+            this.y++;
+        }
+        return {value, done: false};
+    }
+}
+
+Matrix.prototype[Symbol.iterator] = function() {
+    return new MatrixIterator(this);
+};
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+for (let {x, y, value} of matrix) {
+    console.log(x, y, value);
+}
+
+// Getters, setters, statics
+// Can include properties that hold non-function values
+let varyingSize = {
+    get size() {
+        return Math.floor(Math.random() * 100);
+    }
+};
+console.log(varyingSize.size);
+
+// Inheritance
+class SymmetricMatrix extends Matrix {
+    constructor(size, element = (x, y) => undefined) {
+        super(size, size, (x, y) => {
+            if (x < y) return element(y, x);
+            else return element (x, y);
+        })
+    }
+    set(x, y, value) {
+        super.set(x, y, value);
+        if (x != y) { super.set(y, x, value);}
+    }
+}
+
+// instanceof
+console.log(new SymmetricMatrix(2) instanceof SymmetricMatrix);
+console.log(new SymmetricMatrix(2) instanceof Matrix);
+console.log(new Matrix(2, 2) instanceof SymmetricMatrix);
