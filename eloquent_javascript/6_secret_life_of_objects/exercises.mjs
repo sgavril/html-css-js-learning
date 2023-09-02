2dea815854e7273a6307fea63a6882ca4d8c8f59
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-09-01 23:47:53 -0600

    eloquent js ch6: groups

--- eloquent_javascript/6_secret_life_of_objects/exercises.mjs
+++ eloquent_javascript/6_secret_life_of_objects/exercises.mjs
@@ -1,6 +1,48 @@
-// Methods
-let rabbit = {};
-rabbit.speak = function(line) {
-    console.log(`The rabbit says '${line}'`);
+// Vector type
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    plus(vec2) {
+        return new Vec(this.x + vec2.x, this.y + vec2.y);
+    }
+    minus(vec2) {
+        return new Vec(this.x - vec2.x, this.y - vec2.y);
+    }
+    get length() {
+        return Math.sqrt(this.x**2 + this.y**2);
+    }
 }
-rabbit.speak("I'm alive.");
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+console.log(new Vec(3, 4).length);
+
+// Groups
+class Group {
+    constructor() { this.members = []; }
+
+    has(value) { return this.members.includes(value); }
+
+    add(value) { this.has(value) ? value: this.members.push(value); }
+
+    delete(value) {
+        // const index = this.members.indexOf(value);
+        // if (index != -1) {
+        //     this.members.splice(index, 1);
+        // }
+
+        // Using filter
+        this.member = this.members.filter(member => member !== value);
+    }
+
+    static from(iterableObject) {
+        const group = new Group();
+        for (let value of iterableObject) {
+            group.add(value);
+        }
+        return group;
+    }
+}
+
+// Iterable
