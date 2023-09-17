9f672ea7b3bf1d672ffe7f262a7956f3f6e14c8c
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-09-17 14:28:07 -0600

    r2r: jsx functions

--- road_to_react/hacker-stories/src/App.js
+++ road_to_react/hacker-stories/src/App.js
@@ -5,12 +5,14 @@ const welcome = {
   title: 'React',
 };
 
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>Hello {getTitle('React')}</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
