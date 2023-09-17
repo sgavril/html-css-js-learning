a7c2c414db2c7bd950df0cb4dcbd5b31ed63d824
--replace-all <stefan.gavriliuc@ucalgary.ca>
2023-09-17 16:33:34 -0600

    lists in jsx + map function

--- road_to_react/hacker-stories/src/App.js
+++ road_to_react/hacker-stories/src/App.js
@@ -5,6 +5,25 @@ const welcome = {
   title: 'React',
 };
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author:' Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function getTitle(title) {
   return title;
 }
@@ -12,10 +31,18 @@ function getTitle(title) {
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+      <ul>
+        {list.map(function(item) {
+          return <li>{item.title}</li>;
+        })}
+      </ul>
+      {/* Also, this is a comment in JSX */}
     </div>
   );
 }
